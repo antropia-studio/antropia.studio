6438470f63347a1a6d857876155cbfd4dd07c08e
Serchinastico <github@serchinastico.com>
2025-04-16 00:53:29 +0200

    feat: menu

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -7,3 +7,12 @@ test('home', async ({ page }) => {
 
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
+
+test('menu', async ({ page }) => {
+  await page.goto('http://localhost:4321/antropia.studio');
+
+  await page.waitForLoadState('domcontentloaded');
+  await page.locator('#menu-toggle').click({ force: true });
+
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
