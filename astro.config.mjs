882fbc3911c1fa9e07a516ecd1c40d25dcae6d5b
Serchinastico <github@serchinastico.com>
2025-05-21 18:55:11 +0200

    build: fix GH action build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   site: 'https://antropia.studio/',
   vite: {
     plugins: [tailwindcss()],
